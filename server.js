3ee8081f0788e79ef81181e957acff1c80cd6c56
MayuriTashildar <116076521+MayuriTashildar@users.noreply.github.com>
2025-01-17 17:20:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const port = 3019;
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/students");
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Mangodb connection successful");
+});
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const Users = mongoose.model("data", userSchema);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/post", async (req, res) => {
+  const { username, password } = req.body;
+  const user = new Users({
+    username,
+    password,
+  });
+  await user.save();
+  console.log(user);
+  res.send("Form Submission Successful");
+});
+app.listen(port, () => {
+  console.log("server started");
+});
